453e71fc3c7e9eb99f067b2fa4c36c70276f1cc7
Linqi Liu <liulinq@shernet.sheridancollege.ca>
2026-02-02 11:22:04 -0500

    Completed CodingGym Controls 2

--- Assets/Scripts/CodingGym/Controls.cs
+++ Assets/Scripts/CodingGym/Controls.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controls : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     SpriteRenderer spriteRenderer;
+    public Slider rotation;
+    
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -12,11 +15,14 @@ public class Controls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 turn = new Vector3(0, 1, 0);
+        turn.z = rotation.value;
+        transform.eulerAngles = turn;
     }
 
     public void randomColour()
     {
         spriteRenderer.color = Random.ColorHSV();
+        
     }
 }
