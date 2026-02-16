5b4b2e5a4df23c42d672adeeb52ac39c8272665b
Linqi Liu <liulinq@sheridancollege.ca>
2026-02-15 20:25:11 -0500

    Claw goes down when clicking the grab button

---
+++ Assets/Scripts/ToyTask/ClawMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClawMovement : MonoBehaviour
+{
+    public Slider movement;
+    public float farRight;
+    public float farLeft;
+
+    public float topPos = 3.5f;
+    public float bottomPos = -1f;
+    public float t = 0f;
+    public bool grab = false;
+    public float verticalPos = 3.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        movement.maxValue = farRight;
+        movement.minValue = farLeft;
+        movement.value = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grab)
+        {
+            t += Time.deltaTime;
+            verticalPos = Mathf.Lerp(topPos, bottomPos, t);
+
+            if (t > 1f)
+            {
+                grab = false;
+                verticalPos = 3.5f;
+            }
+
+        }
+
+        Vector2 horizontalPos = new Vector2(movement.value, verticalPos);
+        transform.localPosition = horizontalPos;
+    }
+
+    public void GrabPresent()
+    {
+        grab = true;
+        t = 0f;
+    }
+}
